0dabf8acb99d0ef3e85be753ab9a8ae716c7e0f8
satou1990y <usaakirururu@gmail.com>
2023-12-20 17:08:42 +0900

    編集機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,6 +22,12 @@ class TweetsController < ApplicationController
   def edit
     @tweet = Tweet.find(params[:id])
   end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
   
   private
   def tweet_params
