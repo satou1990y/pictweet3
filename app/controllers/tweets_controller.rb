af391c8c776406d9f9dfefa3bbd037d530548731
satou1990y <usaakirururu@gmail.com>
2023-12-20 15:14:57 +0900

    編集画面の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,10 @@ class TweetsController < ApplicationController
     tweet.destroy
     redirect_to root_path
   end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
   
   private
   def tweet_params
